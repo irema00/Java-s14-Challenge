523f8613f9a2a3b805a6270e064a622d4bc7856a
İrem Akın Tunga <iremakin00@gmail.com>
2024-01-24 00:27:10 +0300

    creates hamburger class and defines variables

---
+++ src/model/Hamburger.java
@@ -0,0 +1,11 @@
+package model;
+
+import enums.BreadRollType;
+
+public class Hamburger {
+    private String name;
+   private String meat;
+   private double price;
+   private BreadRollType breadRollType;
+
+}
